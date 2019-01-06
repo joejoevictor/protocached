546f987bc5ba107854ec54dc27e2bae70a58ad3d
Joe Chen <joejoevictor@gmail.com>
2019-01-06 13:26:17 -0800

    Fixing formatting of cpp and BUILD files.
    -Linted cpp files with Google style linter.
    -Removed empty space in BUILD file.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,9 @@
+// Copyright 2019
+// Authors:
+//   "Joe Chen" <joejoevictor@gmail.com>
+//   "Gavin Lin" <gavin.lin.asd@gmail.com>
 #include <iostream>
+
 int main() {
     std::cout << "I am client." << std::endl;
     return 0;
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,4 +1,9 @@
+// Copyright 2019
+// Authors:
+//   "Joe Chen" <joejoevictor@gmail.com>
+//   "Gavin Lin" <gavin.lin.asd@gmail.com>
 #include <iostream>
+
 int main() {
     std::cout << "I am server." << std::endl;
     return 0;
