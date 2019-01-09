e4986a6b5ae2f4ee750f43f139208e7d819c165e
Joe Chen <joejoevictor@gmail.com>
2019-01-09 01:49:56 +0000

    Very basic stub implementation of ProtocachedService server.
    -Added Get/Set API stub
    -Added RunServer function to start server.

--- src/server/main.cc
+++ src/server/main.cc
@@ -11,14 +11,40 @@
 #include "src/proto/protocached.pb.h"
 #include "src/proto/protocached.grpc.pb.h"
 
-void RunServer() {
+// TODO(joejoevictor): Move this to separate header and implementation file
+class ProtocachedServiceImpl final : public protocached::Protocached::Service {
+  grpc::Status Get(
+    grpc::ServerContext* context,
+    const ::protocached::GetRequest* request,
+    protocached::GetResponse* response
+  ) override {
+    protocached::CachedValue cachedValue;
+    cachedValue.set_value("This is a hard-coded value");
+    response -> mutable_value() -> CopyFrom(cachedValue);
+    return grpc::Status::OK;
+  } 
+  
+  grpc::Status Set(
+    grpc::ServerContext* context,
+    const ::protocached::SetRequest* request,
+    protocached::SetResponse* response
+  ) override {
+    return grpc::Status::OK;
+  }
+};
 
+void RunServer() {
+  ProtocachedServiceImpl service;
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort("0.0.0.0:18081", grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on 0.0.0.0:18081" << std::endl;
+  server->Wait();
 }
 
 int main() {
     std::cout << "Starting ProtoCached server." << std::endl;
-
     RunServer();
-
     return 0;
 }
